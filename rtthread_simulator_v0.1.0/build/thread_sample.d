1c65a7ac276b3ab4a85c1e5c83e05bfeb7797dc6
Alva <alvaxu0816@gmail.com>
2025-04-02 23:28:35 +0800

    Initial thread practice

---
+++ rtthread_simulator_v0.1.0/build/thread_sample.d
@@ -0,0 +1,32 @@
+.\build\thread_sample.o: kernel-sample-0.1.0\thread_sample.c
+.\build\thread_sample.o: rt-thread\include\rtthread.h
+.\build\thread_sample.o: .\rtconfig.h
+.\build\thread_sample.o: rt-thread\include\rtdebug.h
+.\build\thread_sample.o: rt-thread\include\rtdef.h
+.\build\thread_sample.o: D:\develop\Keil5 MDK\ARM\ARMCC\Bin\..\include\stdarg.h
+.\build\thread_sample.o: rt-thread\include\libc/libc_signal.h
+.\build\thread_sample.o: D:\develop\Keil5 MDK\ARM\ARMCC\Bin\..\include\signal.h
+.\build\thread_sample.o: rt-thread\include\rtlibc.h
+.\build\thread_sample.o: rt-thread\include\libc/libc_stat.h
+.\build\thread_sample.o: D:\develop\Keil5 MDK\ARM\ARMCC\Bin\..\include\stdint.h
+.\build\thread_sample.o: D:\develop\Keil5 MDK\ARM\ARMCC\Bin\..\include\time.h
+.\build\thread_sample.o: rt-thread\include\libc/libc_errno.h
+.\build\thread_sample.o: rt-thread\include\libc/libc_fcntl.h
+.\build\thread_sample.o: rt-thread\include\libc/libc_ioctl.h
+.\build\thread_sample.o: rt-thread\include\libc/libc_dirent.h
+.\build\thread_sample.o: rt-thread\include\libc/libc_fdset.h
+.\build\thread_sample.o: rt-thread\include\rtservice.h
+.\build\thread_sample.o: rt-thread\include\rtm.h
+.\build\thread_sample.o: rt-thread\include\rtthread.h
+.\build\thread_sample.o: rt-thread\components\finsh\finsh_api.h
+.\build\thread_sample.o: rt-thread\components\drivers\include\rtdevice.h
+.\build\thread_sample.o: rt-thread\components\drivers\include\ipc/ringbuffer.h
+.\build\thread_sample.o: rt-thread\components\drivers\include\ipc/completion.h
+.\build\thread_sample.o: rt-thread\components\drivers\include\ipc/dataqueue.h
+.\build\thread_sample.o: rt-thread\components\drivers\include\ipc/workqueue.h
+.\build\thread_sample.o: rt-thread\components\drivers\include\ipc/waitqueue.h
+.\build\thread_sample.o: rt-thread\components\drivers\include\ipc/pipe.h
+.\build\thread_sample.o: rt-thread\components\drivers\include\rtdevice.h
+.\build\thread_sample.o: rt-thread\components\drivers\include\ipc/poll.h
+.\build\thread_sample.o: rt-thread\components\drivers\include\drivers/serial.h
+.\build\thread_sample.o: rt-thread\components\drivers\include\drivers/pin.h
